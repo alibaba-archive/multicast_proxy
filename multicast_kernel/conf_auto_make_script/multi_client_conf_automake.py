61412ff0627759cbbb71052e5cbf11ed662eb465
xinzaitianshan <xuzhi.xz@alibaba-inc.com>
2017-11-07 11:01:33 +0800

    * let conf auto make to support multi group ip

--- multicast_kernel/conf_auto_make_script/multi_client_conf_automake.py
+++ multicast_kernel/conf_auto_make_script/multi_client_conf_automake.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import json, os, commands, sys
 
-FILENAME="iplist"
+IPLIST_FILE="iplist"
+CONF_FILE="/usr/local/etc/multi_client_running_config"
 ### Format iplist
 ##Line1 multi_ip multi_port
 ##Line2-n normal_ip
@@ -22,11 +23,22 @@ def conf_init(filename):
         print "%s does not exist" %filename
         sys.exit(-1)
 
+    get_info_from_conf(filename)
+
+
+def get_info_from_conf(filename):
+    global json_cfg
     local_vmip = get_local_ip()
 
     i = 0
     multi_ip = ""
     multi_port = ""
+
+    # read exist conf
+    ret = os.path.exists(CONF_FILE)
+    if ret is True:
+        with open(CONF_FILE) as f:
+            json_cfg = json.load(f)
 
     with open(filename) as f:
         lines_info = f.readlines()
@@ -48,7 +60,8 @@ def conf_init(filename):
                 server_info["group_ip"] = multi_ip
                 server_info["client_port"] = multi_port
 
-                json_cfg["multi_server_info_array"].append(server_info)
+                if server_info not in json_cfg["multi_server_info_array"]:
+                    json_cfg["multi_server_info_array"].append(server_info)
 
 
 json_cfg = {
@@ -58,14 +71,19 @@ json_cfg = {
 
 
 if __name__ == "__main__":
-    conf_init(FILENAME)
-    print json_cfg
+    if len(sys.argv) < 2:
+        print "Usage: python %s filename" %(sys.argv[0])
+        sys.exit(1)
+
+    IPLIST_FILE=sys.argv[1]
+    conf_init(IPLIST_FILE)
+
     with open("multi_client_running_config", "w") as dump_f:
         json.dump(json_cfg, dump_f)
 
-    cmd = "cp multi_client_running_config /usr/local/etc/multi_client_running_config"
+    cmd = "mv multi_client_running_config %s" % CONF_FILE
     status, value = commands.getstatusoutput(cmd)
     if (status != 0):
         print "client config auto make error"
 
-    print "client config auto make ok, please restart multic service"
+    print "client config auto make ok, please run <service multic restart>"
--- multicast_kernel/conf_auto_make_script/multi_server_conf_automake.py
+++ multicast_kernel/conf_auto_make_script/multi_server_conf_automake.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import json, os, commands, sys
 
-FILENAME="iplist"
+IPLIST_FILE="iplist"
+CONF_FILE="/usr/local/etc/multi_server_running_config"
 ### Format iplist
 ##Line1 multi_ip, multi_port
 ##Line2-n normal_ip
@@ -22,9 +23,23 @@ def conf_init(filename):
         print "file %s does not exist" %filename
         sys.exit(-1)
 
+    get_info_from_conf(filename)
+
+
+def get_info_from_conf(filename):
+    global json_cfg
+    i = 0
+    server_info = {
+        "group_ip" : "",
+        "member_array" : []
+    }
     local_vmip = get_local_ip()
 
-    i = 0
+    ret = os.path.exists(CONF_FILE)
+    if ret is True:
+        with open(CONF_FILE) as f:
+            json_cfg = json.load(f)
+
     with open(filename) as f:
         lines_info = f.readlines()
         for line in lines_info:
@@ -35,33 +50,37 @@ def conf_init(filename):
                 continue
 
             if i == 0:
-                json_cfg["multi_group_array"][0]["group_ip"] = line.strip('\n').split()[0]
+                server_info["group_ip"] = line.strip('\n').split()[0]
                 i = 1
             else:
                 if local_vmip in line:
                     continue
-                json_cfg["multi_group_array"][0]["member_array"].append(line.strip('\n'))
+                server_info["member_array"].append(line.strip('\n'))
+
+    if server_info not in json_cfg["multi_group_array"]:
+        json_cfg["multi_group_array"].append(server_info)
 
 
 json_cfg = { 
     "multi_group_array" : [
-        {
-            "group_ip" : "",
-            "member_array" : []
-        }    
     ]
 }
 
 
 if __name__ == "__main__":
-    conf_init(FILENAME)
-    print json_cfg
+    if len(sys.argv) < 2:
+        print "Usage: python %s filename" %(sys.argv[0])
+        sys.exit(1)
+
+    IPLIST_FILE=sys.argv[1]
+    conf_init(IPLIST_FILE)
+
     with open("multi_server_running_config", "w") as dump_f:
         json.dump(json_cfg, dump_f)
 
-    cmd = "cp multi_server_running_config /usr/local/etc/multi_server_running_config"
+    cmd = "mv multi_server_running_config %s" %CONF_FILE
     status, value = commands.getstatusoutput(cmd)
     if (status != 0):
         print "server config auto make error"
 
-    print "server config auto make ok, please restart multis service"
+    print "server config auto make ok, please run <service multis restart>"
